494f3c2cdd516e1c6a49b045eb86133a8c489a97
Pavel Lazureykis <lazureykis@gmail.com>
2016-02-18 17:39:06 +0300

    UPD commented out inclustion of IssueTemplatesHelper

--- lib/redmine_issue_template.rb
+++ lib/redmine_issue_template.rb
@@ -5,5 +5,5 @@ require 'redmine_issue_template/hooks/view_hook'
 require 'redmine_issue_template/patches/project_patch'
 require 'redmine_issue_template/patches/projects_helper_patch'
 
-# include IssueTemplatesHelper
+include IssueTemplatesHelper
 require 'redmine_issue_template/patches/application_controller_patch'
